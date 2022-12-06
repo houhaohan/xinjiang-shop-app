6aad54f85bf53c9a4897a57edaf8f1babf616939
houhaohan <751649648@qq.com>
2022-12-06 18:07:14 +0800

    删除无用的校验

--- pinet-rest/src/main/java/com/pinet/rest/entity/Customer.java
+++ pinet-rest/src/main/java/com/pinet/rest/entity/Customer.java
@@ -25,23 +25,18 @@ public class Customer implements Serializable {
     /**
     * 用户id
     */
-    @NotNull(message="[用户id]不能为空")
     @ApiModelProperty("用户id")
     @TableId(value = "customerId",type = IdType.AUTO)
     private Long customerId;
     /**
     * 微信小程序唯一标识
     */
-    @Size(max= 255,message="编码长度不能超过255")
     @ApiModelProperty("微信小程序唯一标识")
-    @Length(max= 255,message="编码长度不能超过255")
     private String openId;
     /**
     * 邀请码
     */
-    @Size(max= 16,message="编码长度不能超过16")
     @ApiModelProperty("邀请码")
-    @Length(max= 16,message="编码长度不能超过16")
     private String uuid;
     /**
     * 邀请我的人
@@ -51,50 +46,37 @@ public class Customer implements Serializable {
     /**
     * 密码(MD5(密码))
     */
-    @Size(max= 100,message="编码长度不能超过100")
     @ApiModelProperty("密码(MD5(密码))")
-    @Length(max= 100,message="编码长度不能超过100")
     private String password;
     /**
     * 昵称
     */
-    @Size(max= 20,message="编码长度不能超过20")
     @ApiModelProperty("昵称")
-    @Length(max= 20,message="编码长度不能超过20")
     private String nickname;
     /**
     * 
     */
-    @Size(max= 16,message="编码长度不能超过16")
     @ApiModelProperty("")
-    @Length(max= 16,message="编码长度不能超过16")
     private String realname;
     /**
     * 手机号
     */
-    @Size(max= 16,message="编码长度不能超过16")
     @ApiModelProperty("手机号")
-    @Length(max= 16,message="编码长度不能超过16")
     private String phone;
     /**
     * 性别(0:未知,1:男,2:女)
     */
-    @NotNull(message="[性别(0:未知,1:男,2:女)]不能为空")
     @ApiModelProperty("性别(0:未知,1:男,2:女)")
     private Integer sex;
     /**
     * 头像
     */
-    @Size(max= 512,message="编码长度不能超过512")
     @ApiModelProperty("头像")
-    @Length(max= 512,message="编码长度不能超过512")
     private String avatar;
     /**
     * 
     */
-    @Size(max= 64,message="编码长度不能超过64")
     @ApiModelProperty("")
-    @Length(max= 64,message="编码长度不能超过64")
     private String email;
     /**
     * 
@@ -109,7 +91,6 @@ public class Customer implements Serializable {
     /**
     * 1-可用，0-禁用
     */
-    @NotNull(message="[1-可用，0-禁用]不能为空")
     @ApiModelProperty("1-可用，0-禁用")
     private Integer active;
     /**
@@ -125,22 +106,17 @@ public class Customer implements Serializable {
     /**
     * 区域
     */
-    @Size(max= 32,message="编码长度不能超过32")
     @ApiModelProperty("区域")
-    @Length(max= 32,message="编码长度不能超过32")
     private String area;
     /**
     * 注册时间
     */
-    @NotNull(message="[注册时间]不能为空")
     @ApiModelProperty("注册时间")
     private Long createTime;
     /**
     * 注册IP地址
     */
-    @Size(max= 50,message="编码长度不能超过50")
     @ApiModelProperty("注册IP地址")
-    @Length(max= 50,message="编码长度不能超过50")
     private String createIp;
     /**
     * 最后登录时间
@@ -150,23 +126,17 @@ public class Customer implements Serializable {
     /**
     * 最后登录IP地址
     */
-    @Size(max= 50,message="编码长度不能超过50")
     @ApiModelProperty("最后登录IP地址")
-    @Length(max= 50,message="编码长度不能超过50")
     private String lastLoginIp;
     /**
     * 极光推送token
     */
-    @Size(max= 128,message="编码长度不能超过128")
     @ApiModelProperty("极光推送token")
-    @Length(max= 128,message="编码长度不能超过128")
     private String jpushToken;
     /**
     * 云信的token
     */
-    @Size(max= 128,message="编码长度不能超过128")
     @ApiModelProperty("云信的token")
-    @Length(max= 128,message="编码长度不能超过128")
     private String yxToken;
     /**
     * 终端
@@ -196,16 +166,12 @@ public class Customer implements Serializable {
     /**
     * 支付密码
     */
-    @Size(max= 32,message="编码长度不能超过32")
     @ApiModelProperty("支付密码")
-    @Length(max= 32,message="编码长度不能超过32")
     private String payPassword;
     /**
     * 环信uuid
     */
-    @Size(max= 40,message="编码长度不能超过40")
     @ApiModelProperty("环信uuid")
-    @Length(max= 40,message="编码长度不能超过40")
     private String hxUuid;
     /**
     * 是否设置支付密码，1为已设置，0为未设置
@@ -215,25 +181,21 @@ public class Customer implements Serializable {
     /**
     * 是否为新用户 0：新用户 1：老用户
     */
-    @NotNull(message="[是否为新用户 0：新用户 1：老用户]不能为空")
     @ApiModelProperty("是否为新用户 0：新用户 1：老用户")
     private Integer isNew;
     /**
     * 元宵节猜灯谜 当天是否领取了奖项 0为未领取 1为领取了
     */
-    @NotNull(message="[元宵节猜灯谜 当天是否领取了奖项 0为未领取 1为领取了]不能为空")
     @ApiModelProperty("元宵节猜灯谜 当天是否领取了奖项 0为未领取 1为领取了")
     private Integer ridIsReceive;
     /**
     * 当天答题次数
     */
-    @NotNull(message="[当天答题次数]不能为空")
     @ApiModelProperty("当天答题次数")
     private Integer ridAnswerNum;
     /**
     *  是否为全民合伙人 0.否 1.是
     */
-    @NotNull(message="[ 是否为全民合伙人 0.否 1.是]不能为空")
     @ApiModelProperty(" 是否为全民合伙人 0.否 1.是")
     private Integer isPartner;
     /**
--- pinet-rest/src/main/java/com/pinet/rest/entity/CustomerAddress.java
+++ pinet-rest/src/main/java/com/pinet/rest/entity/CustomerAddress.java
@@ -34,85 +34,61 @@ public class CustomerAddress implements Serializable {
     /**
     * 省
     */
-    @NotNull(message="[省]不能为空")
     @ApiModelProperty("省")
     private Integer provinceId;
     /**
     * 市
     */
-    @NotNull(message="[市]不能为空")
     @ApiModelProperty("市")
     private Integer cityId;
     /**
     * 区
     */
-    @NotNull(message="[区]不能为空")
     @ApiModelProperty("区")
     private Integer districtId;
     /**
     * 姓名
     */
-    @NotBlank(message="[姓名]不能为空")
-    @Size(max= 64,message="编码长度不能超过64")
     @ApiModelProperty("姓名")
-    @Length(max= 64,message="编码长度不能超过64")
     private String name;
     /**
     * 
     */
-    @Size(max= 255,message="编码长度不能超过255")
     @ApiModelProperty("")
-    @Length(max= 255,message="编码长度不能超过255")
     private String address;
     /**
     * 
     */
-    @NotBlank(message="[]不能为空")
-    @Size(max= 32,message="编码长度不能超过32")
     @ApiModelProperty("")
-    @Length(max= 32,message="编码长度不能超过32")
     private String province;
     /**
     * 
     */
-    @NotBlank(message="[]不能为空")
-    @Size(max= 128,message="编码长度不能超过128")
     @ApiModelProperty("")
-    @Length(max= 128,message="编码长度不能超过128")
     private String city;
     /**
     * 
     */
-    @NotBlank(message="[]不能为空")
-    @Size(max= 128,message="编码长度不能超过128")
     @ApiModelProperty("")
-    @Length(max= 128,message="编码长度不能超过128")
     private String district;
     /**
     * 邮编
     */
-    @Size(max= 12,message="编码长度不能超过12")
     @ApiModelProperty("邮编")
-    @Length(max= 12,message="编码长度不能超过12")
     private String postcode;
     /**
     * 手机号码
     */
-    @Size(max= 32,message="编码长度不能超过32")
     @ApiModelProperty("手机号码")
-    @Length(max= 32,message="编码长度不能超过32")
     private String phone;
     /**
     * 电话
     */
-    @Size(max= 32,message="编码长度不能超过32")
     @ApiModelProperty("电话")
-    @Length(max= 32,message="编码长度不能超过32")
     private String tel;
     /**
     * 
     */
-    @NotNull(message="[]不能为空")
     @ApiModelProperty("")
     private Integer active;
     /**
@@ -123,13 +99,11 @@ public class CustomerAddress implements Serializable {
     /**
     * 
     */
-    @NotNull(message="[]不能为空")
     @ApiModelProperty("")
     private Long createTime;
     /**
     * 
     */
-    @NotNull(message="[]不能为空")
     @ApiModelProperty("")
     private Long updateTime;
     /**
@@ -145,16 +119,12 @@ public class CustomerAddress implements Serializable {
     /**
     * 门牌号
     */
-    @Size(max= 256,message="编码长度不能超过256")
     @ApiModelProperty("门牌号")
-    @Length(max= 256,message="编码长度不能超过256")
     private String houseNumber;
     /**
     * 地点名
     */
-    @Size(max= 256,message="编码长度不能超过256")
     @ApiModelProperty("地点名")
-    @Length(max= 256,message="编码长度不能超过256")
     private String addressName;
 
 }
