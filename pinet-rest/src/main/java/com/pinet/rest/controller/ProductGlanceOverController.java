06a9933ba606bdb244b5cdd88ef0f16832c7ac1c
chengshuanghui <1012033068@qq.com>
2022-12-08 17:47:59 +0800

    商品流览

---
+++ pinet-rest/src/main/java/com/pinet/rest/controller/ProductGlanceOverController.java
@@ -0,0 +1,21 @@
+package com.pinet.rest.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.pinet.core.controller.BaseController;
+
+/**
+ * <p>
+ * 商品浏览表 前端控制器
+ * </p>
+ *
+ * @author wlbz
+ * @since 2022-12-08
+ */
+@RestController
+@RequestMapping("/product/glanceOver")
+public class ProductGlanceOverController extends BaseController {
+
+}
---
+++ pinet-rest/src/main/java/com/pinet/rest/service/impl/ProductGlanceOverServiceImpl.java
@@ -0,0 +1,20 @@
+package com.pinet.rest.service.impl;
+
+import com.pinet.rest.entity.ProductGlanceOver;
+import com.pinet.rest.mapper.ProductGlanceOverMapper;
+import com.pinet.rest.service.IProductGlanceOverService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 商品浏览表 服务实现类
+ * </p>
+ *
+ * @author wlbz
+ * @since 2022-12-08
+ */
+@Service
+public class ProductGlanceOverServiceImpl extends ServiceImpl<ProductGlanceOverMapper, ProductGlanceOver> implements IProductGlanceOverService {
+
+}
