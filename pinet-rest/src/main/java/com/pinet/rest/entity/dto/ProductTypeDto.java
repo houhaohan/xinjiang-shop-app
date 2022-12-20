a6b3c8775b79f6cbfaaa8f3e57759e4cadf90002
白京 <990429235@qq.com>
2022-12-20 14:57:34 +0800

    修改店铺商品列表接口

--- pinet-rest/src/main/java/com/pinet/rest/entity/dto/ProductTypeDto.java
+++ pinet-rest/src/main/java/com/pinet/rest/entity/dto/ProductTypeDto.java
@@ -1,15 +1,25 @@
 package com.pinet.rest.entity.dto;
 
 import com.pinet.rest.entity.common.CommonPage;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 /**
  * @author: bj
  * @create: 2022/12/12 17:07
  */
 @Data
-public class ProductTypeDto extends CommonPage {
+public class ProductTypeDto {
+    @ApiModelProperty(value = "店铺ID")
     private Long shopId;
+
+    @ApiModelProperty(value = "纬度")
+    private BigDecimal lat;
+
+    @ApiModelProperty(value = "经度")
+    private BigDecimal lng;
 
 
 }
--- pinet-rest/src/main/java/com/pinet/rest/service/impl/ProductTypeServiceImpl.java
+++ pinet-rest/src/main/java/com/pinet/rest/service/impl/ProductTypeServiceImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.ObjectUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.pinet.rest.entity.ProductType;
+import com.pinet.rest.entity.Shop;
 import com.pinet.rest.entity.ShopProduct;
 import com.pinet.rest.entity.dto.ProductTypeDto;
 import com.pinet.rest.entity.vo.ProductTypeVo;
@@ -11,10 +12,12 @@ import com.pinet.rest.mapper.ProductTypeMapper;
 import com.pinet.rest.mapper.ShopProductMapper;
 import com.pinet.rest.service.IProductTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.pinet.rest.service.IShopService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -37,11 +40,22 @@ public class ProductTypeServiceImpl extends ServiceImpl<ProductTypeMapper, Produ
     @Autowired
     private ProductTypeMapper productTypeMapper;
 
+    @Autowired
+    private IShopService shopService;
+
     @Override
     public List<ProductTypeVo> productType(ProductTypeDto dto) {
+        if(dto.getLat() == null && dto.getLng() == null){
+            throw new IllegalArgumentException("参数不能为空");
+        }
+        //根据经纬度获取最近的店铺ID
+        Shop shop = shopService.getMinDistanceShop(dto.getLat(), dto.getLng());
+        if(shop == null){
+            return Collections.emptyList();
+        }
         //查点店铺商品
         List<ShopProduct> shopProductList = shopProductMapper.selectList(Wrappers.lambdaQuery(new ShopProduct())
-                .eq(ShopProduct::getShopId, dto.getShopId())
+                .eq(ShopProduct::getShopId, shop.getId())
         );
         //根据商品类型分类
         Map<Long, List<ShopProduct>> typeProductMap = null;
