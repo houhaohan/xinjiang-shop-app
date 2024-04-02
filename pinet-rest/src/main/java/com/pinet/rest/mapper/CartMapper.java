29a4ebf6df3dbe09bedd47de2ee9fb123b1f859b
chengshuanghui <xiaohuihui5566@gmail.com>
2024-04-03 00:08:01 +0800

    xx

--- pinet-rest/src/main/java/com/pinet/rest/mapper/CartMapper.java
+++ pinet-rest/src/main/java/com/pinet/rest/mapper/CartMapper.java
@@ -7,7 +7,6 @@ import com.pinet.rest.entity.vo.CartListVo;
 import com.pinet.rest.entity.vo.CartVo;
 import org.apache.ibatis.annotations.Param;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -38,17 +37,5 @@ public interface CartMapper extends BaseMapper<Cart> {
      */
     Cart getByUserIdAndShopProdId(@Param("customerId") Long customerId, @Param("shopProdId") Long shopProdId);
 
-    /**
-     * 查询购物车单品价格 数量
-     * @param cartId
-     * @return
-     */
-    BigDecimal getSingleByCartId(@Param("cartId") Long cartId);
 
-    /**
-     * 查询购物车单品价格 数量
-     * @param cartId
-     * @return
-     */
-    Long getComboByCartId(@Param("cartId") Long cartId);
 }
